91d21680431e9a40ff7350171ed65d7cc1889b5c
sonia <s.rolland@codeur.online>
2021-04-27 10:45:07 +0200

    first

---
+++ main.js
@@ -0,0 +1,19 @@
+const chart = document.getElementById('chart');
+
+let array = [];
+for(let i =0; i<100; i++){
+    let number = Math.floor(Math.random()*100);
+    array.push(number);
+    
+}
+  function draw(){
+     chart.innerHTML = '';
+     array.forEach(function(x){
+         let div = document.createElement('div');
+         div.style.height = x+ 'px';
+         chart.appendChild(div);
+     })
+        
+
+  }
+  draw();
